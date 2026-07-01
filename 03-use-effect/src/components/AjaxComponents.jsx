427889cabcb983388634e287298e86e14c1c55ec
omarescobartorres10 <omar.escobartorres10@gmail.com>
2026-07-01 09:46:35 -0600

    feat: react y asincronia

--- 03-use-effect/src/components/AjaxComponents.jsx
+++ 03-use-effect/src/components/AjaxComponents.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
 
 export const AjaxComponents = () => {
-    const [usuarios, setUsarios] = useState([]) //estados de usuarios
+    const [usuarios, setUsuarios] = useState([]) //estados de usuarios
+    // const [usuarios, setUsuarios] = useState([]);
     const getUsuariosEstatitivos = () => { //array de objetos con usuarios
-        setUsarios([
+        setUsuarios([
             {
                 "id": 1,
                 "email": "omar@gmail.com",
@@ -23,35 +24,50 @@ export const AjaxComponents = () => {
         ])
     }
 
+    const getUsuariosAwait = async () => {
+    const peticion = await fetch("https://reqres.in/api/users?page=1");
+    const resultado = await peticion.json();
+    
+    setUsuarios(resultado.data);
 
+    console.log(resultado)
+};
 
+    // const getUsuariosAjax = () => {
+    //     fetch("https://api.thecatapi.com/v1/images/search")
+    //         .then(res => res.json())
+    //         .then(resultado => {
+    //             setUsuarios(resultado);
+    //         });
+    //         console.log(usuarios)
+    // };
 
     useEffect(() => {
-        getUsuariosEstatitivos() 
+        // getUsuariosEstatitivos() 
+        //getUsuariosAjax()
+        getUsuariosAwait()
     }, [])
 
     //se renderiza el componente por primera vez
     //DESPUÉS DEL RENDERIZADO se ejecuta el useEffect
     //en el useEffect se ejecuta getUsuariosEstatitivos() 
     //dentro de getUsuariosEstatitivos() se llama setUsarios
-    //eso actualiza el estado y react-re renderiza ek componente con la nueva lista de usuarios
+    // eso actualiza el estado y react-re renderiza ek componente con la nueva lista de usuarios
 
 
-  return (
-    <div>
-        <h2>Listado de usuarios vía AJAX</h2>
-        <ol>
-            {
-                usuarios.map(usuario => {
-                    return <li key={usuario.id}>{usuario.first_name}  {usuario.last_name}
-
+    return (
+        <div>
+            <h2>Listado de usuarios vía AJAX</h2>
+            <ol>
+                {usuarios.map(usuario => (
+                    <li key={usuario.id}>
+                        {usuario.first_name} {usuario.last_name}
                     </li>
-                } )
-            }
-        </ol>
+                ))}
+            </ol>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 
